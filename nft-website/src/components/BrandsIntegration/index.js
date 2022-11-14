1702d74303703601dce3b7c62e82ae92a3ccbc80
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-11-14 13:07:40 +0530

    Initial React App of NFT Website

---
+++ nft-website/src/components/BrandsIntegration/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BrandsIntegration = () => {
+  return <div>BrandsIntegration</div>;
+};
+
+export default BrandsIntegration;
---
+++ nft-website/src/components/TrendingNFts/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./trendingNFts.css";
+
+const TrendingNFts = () => {
+  return <div>TrendingNFts</div>;
+};
+
+export default TrendingNFts;
